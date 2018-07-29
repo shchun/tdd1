0b8aa58c79b3d32bd83da65f3deadf24824bbc6a
Seunghyo Chun <seunghyo.chun@gmail.com>
2018-07-30 02:24:09 +0900

    add Sum

---
+++ src/main/java/com/precipi/Sum.java
@@ -0,0 +1,12 @@
+package com.precipi;
+
+class Sum implements Expression {
+    Money augend;
+    Money addend;
+
+    Sum(Money augend, Money addend) {
+        this.augend = augend;
+        this.addend = addend;
+
+    }
+}
