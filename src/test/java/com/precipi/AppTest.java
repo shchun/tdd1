3485078bbed1793d679e962994d3e287f8e4a353
Seunghyo Chun <seunghyo.chun@gmail.com>
2018-07-30 03:35:13 +0900

    add mixed currencies

--- src/test/java/com/precipi/AppTest.java
+++ src/test/java/com/precipi/AppTest.java
@@ -77,5 +77,16 @@ public class AppTest
     public void testIdentityRate() {
         assertEquals(1, new Bank().rate("USD","USD"));
     }
+    @Test
+    public void testMixedAddition() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrans = Money.franc(10);
 
+        Bank bank = new Bank();
+
+        bank.addRate("CHF","USD", 2);
+        Money result = bank.reduce(fiveBucks.plus(tenFrans),"USD");
+        assertEquals(Money.dollar(10), result);
+
+    }
 }
